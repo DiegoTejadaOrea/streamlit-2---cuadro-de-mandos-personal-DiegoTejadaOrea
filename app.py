2b5a98745002fc3bd7bab2cea46d290c1c167675
Diego Tejada <tejadaoreadiego@gmail.com>
2023-02-27 18:23:09 +0100

    filtro: generacion, salud y barra de progreso de carga

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
+import time
 import pandas as pd
 import streamlit as st
 
 # URL con la lista de pokemones
 URL = 'https://gist.githubusercontent.com/armgilles/194bcff35001e7eb53a2a8b441e8b2c6/raw/92200bc0a673d5ce2110aaad4544ed6c4010f687/pokemon.csv'
-
-
-# Using object notation
-add_selectbox = st.sidebar.selectbox(
-    "How would you like to be contacted?",
-    ("#", "Name", "Type 1", "Generation")
-)
 
 
 @st.cache_data
@@ -21,5 +15,37 @@ def load_data():
 
 pokemon_data = load_data()
 
+
+# SELECCIONAR LA GENERACON DE POKEMON
+def select_generation():
+    st.sidebar.header("Selecciona la generación")
+    st.sidebar.slider(
+        "La generaciones van de la 1 a la 6", min_value=1, max_value=6)
+
+
+# SELECCIONAR VALOR VIDA
+st.sidebar.header("Selecciona el rango de vida")
+columnas_hp = st.sidebar.columns(2)
+hp_min = columnas_hp[0].number_input(
+    "Salud minima", value=pokemon_data['HP'].min())
+HP_max = columnas_hp[1].number_input(
+    "Maximum HP", value=pokemon_data['HP'].max())
+if HP_max < hp_min:
+    st.error("La salud máxima no puede ser menor que la mínima!")
+else:
+    st.success("Parametros correctos!")
+    subset_HP = pokemon_data[(pokemon_data['HP'] <= HP_max) & (
+        hp_min <= pokemon_data['HP'])]
+
+# BARRA DE CARGA
+progress_bar = st.progress(0)
+progress_text = st.empty()
+# definir el tiempo de espera
+for i in range(101):
+    time.sleep(0.1)
+    progress_bar.progress(i)
+    progress_text.text(f"Progress: {i}%")
+
+
 st.header('Pokemones')
-st.write(pokemon_data)
+st.write(select_generation())
